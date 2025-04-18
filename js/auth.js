f8c1960d4f3c97d092454113a1adc3eaf382dfef
mkzer <46260880+mkzer@users.noreply.github.com>
2025-04-18 19:06:06 +0200

    correction bug affichage

--- js/auth.js
+++ js/auth.js
@@ -13,24 +13,7 @@ document.querySelector("form").addEventListener("submit", async (e) => {
         alert("Connexion réussie !");
         localStorage.setItem("token", result.token);
         localStorage.setItem("utilisateurId", result.utilisateur.id);
-
-        // ✅ Normalisation du parcours
-        const parcoursBrut = result.utilisateur.parcours;
-        let parcoursNormalise = parcoursBrut;
-
-        if (parcoursBrut === "M1 EEA MTI") parcoursNormalise = "MTI";
-        if (parcoursBrut === "M1 EEA ISHM") parcoursNormalise = "ISHM";
-        if (parcoursBrut === "M1 EEA IMEEN") parcoursNormalise = "IMEEN";
-
-        result.utilisateur.parcours = parcoursNormalise;
-
         localStorage.setItem("utilisateur", JSON.stringify(result.utilisateur));
-
-        // Appel de l'initialisation automatique des notes
-        await apiFetch("notes/init", {
-            method: "POST",
-            headers: { Authorization: `Bearer ${result.token}` }
-        });
 
         window.location.href = "dashboard.html";
     } catch (err) {
